9841706b582dd494097f37cf2f61a3e0da47add9
Boramat Tangwiwat <boramat@Boramats-MacBook-Air.local>
2021-11-03 09:15:42 +0700

    generic traits lifetime

---
+++ gtl/target/debug/deps/gtl-bd54a08ba68f4d6f.d
@@ -0,0 +1,5 @@
+/Users/boramat/git/rust_projects/gtl/target/debug/deps/gtl-bd54a08ba68f4d6f: src/main.rs
+
+/Users/boramat/git/rust_projects/gtl/target/debug/deps/gtl-bd54a08ba68f4d6f.d: src/main.rs
+
+src/main.rs:
---
+++ gtl/target/debug/gtl.d
@@ -0,0 +1,1 @@
+/Users/boramat/git/rust_projects/gtl/target/debug/gtl: /Users/boramat/git/rust_projects/gtl/src/main.rs
