a10b9cbd374e06c1bc8035796bc5b09f1fb4cb40
Boramat Tangwiwat <boramat@Boramats-MacBook-Air.local>
2021-11-04 17:29:10 +0700

    test

---
+++ adder/target/debug/deps/adder-086d1f697f5f2650.d
@@ -0,0 +1,5 @@
+/Users/boramat/git/rust_projects/adder/target/debug/deps/adder-086d1f697f5f2650: src/lib.rs
+
+/Users/boramat/git/rust_projects/adder/target/debug/deps/adder-086d1f697f5f2650.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-a0128ccf54cf416d.d
@@ -0,0 +1,7 @@
+/Users/boramat/git/rust_projects/adder/target/debug/deps/adder-a0128ccf54cf416d.rmeta: src/lib.rs
+
+/Users/boramat/git/rust_projects/adder/target/debug/deps/libadder-a0128ccf54cf416d.rlib: src/lib.rs
+
+/Users/boramat/git/rust_projects/adder/target/debug/deps/adder-a0128ccf54cf416d.d: src/lib.rs
+
+src/lib.rs:
